8bbf5c6c3b397b330d2b894ba1239657af494b0e
Justyna <Fitec@HPG5-114>
2019-02-06 12:36:04 +0100

    CreationCompte, Creation sans Conditions, a partir de Home/CreateAccount

---
+++ src/ContosoUniversity/Controllers/CreateAccountController.cs
@@ -0,0 +1,70 @@
+﻿using ContosoUniversity.DAL;
+using ContosoUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace ContosoUniversity.Controllers
+{
+    public class CreateAccountController : Controller
+    {
+        SchoolContext db = new SchoolContext();
+
+        // GET: CreateAccount
+        [HttpGet]
+        public ActionResult CreateAccount()
+        {
+            List<string> accountTypes = new List<string>();
+            accountTypes.Add("Student");
+            accountTypes.Add("Instructor");
+            ViewBag.Message = accountTypes;
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult CreateAccount([Bind(Include = "AccountType")] string accountType, string LastName, string FirstMidName, string MailAdresse, string Login, string Password)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            if (ModelState.IsValid)
+            {
+                if (accountType == "Instructor")
+                {
+                    Instructor instructor = new Instructor();
+                    instructor.LastName = LastName;
+                    instructor.FirstMidName = FirstMidName;
+                    instructor.MailAdresse = LastName;
+                    instructor.Login = Login;
+                    instructor.Password = Password;
+                    instructor.HireDate = DateTime.Parse(DateTime.Now.ToString());
+                    db.Instructors.Add(instructor);
+                    db.SaveChanges();
+                    //instructor.ID est recupere ici
+                    return RedirectToAction("Index", "Instructor");
+                }
+                if (accountType == "Student")
+                {
+                    Student student = new Student();
+                    student.LastName = LastName;
+                    student.FirstMidName = FirstMidName;
+                    student.MailAdresse = LastName;
+                    student.Login = Login;
+                    student.Password = Password;
+                    student.EnrollmentDate = DateTime.Parse(DateTime.Now.ToString());
+                    db.Students.Add(student);
+                    db.SaveChanges();
+                    return RedirectToAction("Index", "Student");
+                }
+                else return View();
+           }
+
+            return View();
+        }
+    }
+}
--- src/ContosoUniversity/Models/Person.cs
+++ src/ContosoUniversity/Models/Person.cs
@@ -1,8 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using ContosoUniversity.ViewModels;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace ContosoUniversity.Models
 {
+    public enum DiscriminatorOptions
+    {
+        Student = 1,
+        Instructor = 2,
+    }
+
     public abstract class Person
     {
         public int ID { get; set; }
@@ -11,11 +19,23 @@ namespace ContosoUniversity.Models
         [StringLength(50)]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
         [Required]
         [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         [Column("FirstName")]
         [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
+
+        //Email (expressions regulieres a ajouter)
+        public string MailAdresse { get; set; }
+
+        //Login
+        public string Login { get; set; }
+
+        //Password
+        public string Password { get; set; }
+
+
 
         [Display(Name = "Full Name")]
         public string FullName
