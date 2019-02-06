84f86dc5219bf61f199f51e3b99d4de5f391f5b8
Matoula Petrolia <matoula.p@gmail.com>
2019-02-06 15:11:47 +0100

    Comments and Button

--- src/ContosoUniversity/Controllers/LoginController.cs
+++ src/ContosoUniversity/Controllers/LoginController.cs
@@ -11,13 +11,14 @@ namespace ContosoUniversity.Controllers
     public class LoginController : Controller
     {
         // GET: Login 
-        // Returns only the view with the form to be filled
+        // Returns only the view with the form to be filled to login
         public ActionResult Login()
         {
 
             return View();
         }
 
+        // POST: Takes the login and password we entered in GET and allows the user to login
         [HttpPost]
         public ActionResult Login(string login, string password)
         {
@@ -26,25 +27,28 @@ namespace ContosoUniversity.Controllers
                 //FirstOrDefault returns a person, check if it is a student or instructor
               
                 if (db.People.FirstOrDefault(u => u.Login == login && u.Password == password) is Student)
-                {
+                {   // Store login and password in a new empty student
                     Student user = new Student();
                     user.Login = login;
                     user.Password = password;
                     //Session is not empty, it means you are connected
                     Session["UserName"] = user;
-
+                    // Redirects you to your HomePage
                     return RedirectToAction("Index", "Student");
                 }
                 else if (db.People.FirstOrDefault(u => u.Login == login && u.Password == password) is Instructor)
                 {
+                    // Store login and password in a new empty instructor
                     Instructor user = new Instructor();
                     user.Login = login;
                     user.Password = password;
+                    //Session is not empty, it means you are connected
                     Session["UserName"] = user;
+                    // Redirects you to your HomePage
                     return RedirectToAction("Index", "Instructor");
                 }
                 else
-                {
+                {   // Prints error message
                     TempData["error"] = "Invalid Login or Password";
                 }
 
