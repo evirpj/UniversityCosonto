957f848492ea7aca2ecb7444e1fd2d907e14689a
Matoula Petrolia <matoula.p@gmail.com>
2019-02-08 13:24:08 +0100

    Creation Empty Tests for Login

---
+++ tests/ContosoUniversity.Tests/Controllers/LoginControllerTests.cs
@@ -0,0 +1,55 @@
+﻿using ContosoUniversity.Tests.Tools;
+using ContosoUniversity.Controllers;
+using ContosoUniversity.DAL;
+using ContosoUniversity.Models;
+using Moq;
+using NUnit.Framework;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+
+namespace ContosoUniversity.Tests.Controllers
+{
+    class LoginControllerTests : IntegrationTestsBase
+    {
+        private MockHttpContextWrapper httpContext;
+        private LoginController controllerToTest;
+        private SchoolContext dbContext;
+
+
+        [SetUp]
+        public void Initialize()
+        {
+            httpContext = new MockHttpContextWrapper();
+            controllerToTest = new LoginController();
+            controllerToTest.ControllerContext = new ControllerContext(httpContext.Context.Object, new RouteData(), controllerToTest);
+            dbContext = new DAL.SchoolContext(this.ConnectionString);
+            controllerToTest.DbContext = dbContext;
+        }
+
+
+        [Test]
+        public void LoginSuccess()
+        {
+            
+
+        }
+
+
+
+
+        [Test]
+        public void LoginFail()
+        {
+
+        }
+
+        [Test]
+        public void SuccesfulLogout()
+        {
+
+        }
+
+    }
+}
